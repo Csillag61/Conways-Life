b7b02c3ec613374ba6e06ac2c6cbff4770b04a92
Csillag61 <csillagok@gmail.com>
2019-03-20 18:29:11 -0400

    Updates

---
+++ js/headingAnimate.js
@@ -0,0 +1,21 @@
+const heading = document.querySelector(".whoWeAre__container h2");
+
+heading.addEventListener(
+  "mouseover",
+  () => (heading.style.borderBottom = "2px dotted black")
+);
+
+heading.addEventListener(
+  "mouseout",
+  () => (heading.style.borderBottom = "none")
+);
+
+//SPAN ANIMATE STYLING
+const span = document.querySelector(".hero__intro span");
+
+span.addEventListener(
+  "mouseover",
+  () => (span.style.borderBottom = "2px solid white")
+);
+
+span.addEventListener("mouseout", () => (span.style.borderBottom = "none"));
---
+++ precache-manifest.d8cfb022edf2560335a2c9785911b98e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40d0d8b58644d98765a1",
+    "url": "/Conways-Life/static/css/main.af1378b9.chunk.css"
+  },
+  {
+    "revision": "40d0d8b58644d98765a1",
+    "url": "/Conways-Life/static/js/main.175f587a.chunk.js"
+  },
+  {
+    "revision": "f94638f27adfa8c548ad",
+    "url": "/Conways-Life/static/js/runtime~main.5602915e.js"
+  },
+  {
+    "revision": "5b33e73b8317db784d75",
+    "url": "/Conways-Life/static/js/2.57ac86a3.chunk.js"
+  },
+  {
+    "revision": "fdca77cc7f27712f731f5ac03e33016d",
+    "url": "/Conways-Life/index.html"
+  }
+];
